f5cfc48520dc381db6133ce11377584e76914e6d
Meghana Nayak <meghanayakmeghanayak90@gmail.com>
2026-03-06 13:00:24 +0530

    push

---
+++ ai_teacher/chatbot/views.py
@@ -0,0 +1,27 @@
+
+
+# Create your views here.
+from django.shortcuts import render
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def home(request):
+
+    answer = ""
+
+    if request.method == "POST":
+        question = request.POST.get("question")
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=question
+        )
+
+        answer = response.text
+
+    return render(request, "index.html", {"answer": answer})
