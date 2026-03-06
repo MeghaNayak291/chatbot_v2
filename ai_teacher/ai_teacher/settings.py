5caaa96df06e2d62d880a0abb773f99cd6f4b4a0
Meghana Nayak <meghanayakmeghanayak90@gmail.com>
2026-03-06 14:03:30 +0530

    push

--- ai_teacher/ai_teacher/settings.py
+++ ai_teacher/ai_teacher/settings.py
@@ -31,10 +31,8 @@ SECRET_KEY = 'django-insecure-bxog^cbky)3!=&$_@do)y7kt#o_g=_zq_moxnggj=j$t@nh*gw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "chatbot-v2-1-hr2t.onrender.com",
-]
-    
+ALLOWED_HOSTS = ["*"]
+
 # Application definition
 
 INSTALLED_APPS = [
